16aa0caa8cd979227fe19a9ed8748e881df7cedf
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-04-16 15:31:38 +0200

    progress on that helper cf #5

--- R/blog_posts.R
+++ R/blog_posts.R
@@ -42,9 +42,16 @@ create_post_project <- function(slug, type = "Rmd"){
     rmarkdown::render(post_path)
   }
 
-  readme_template <- system.file("templates/post_readme.md",
-                               package = "rodev")
-  file.copy(readme_template, file.path(getwd(), slug, "README.md"))
+  readme_template <- readLines(system.file("templates/post_readme.md",
+                               package = "rodev"))
+  if(type == "Rmd"){
+    readme_template[3] <- "See [the blog post source here](post.Rmd) and its rendered version [here](post.md)." # nolint
+  } else{
+    readme_template[3] <- "See the blog post [here](post.md)"
+  }
+
+
+  writeLines(readme_template, file.path(getwd(), slug, "README.md"))
 
   if(rstudioapi::isAvailable()){
     rstudioapi::openProject(file.path(getwd(), slug))
@@ -52,4 +59,31 @@ create_post_project <- function(slug, type = "Rmd"){
     message(paste("Project files created in"),
             file.path(getwd(), slug))
   }
+}
+
+
+prepare_fork <- function(post_project_path, gh_username = "maelle"){
+  # if fork exist update it
+
+  # put post in right folder
+  # with name deduced from date and slug
+  post_project_path="C:/Users/Maelle/Documents/yay"
+  post_path <- file.path(post_project_path, "post.md")
+  post_yaml <- yaml::read_yaml(post_path)
+  slug <- post_yaml$slug
+  date <- post_yaml$date
+  file_name <- paste0(date, "-", slug, ".md")
+
+  # replace image links
+
+  post_content <- readBin(post_path, "raw",
+                                   file.info(post_path)[1, "size"])
+
+  gh::gh("PUT /repos/:owner/:repo/contents/:path",
+         owner = gh_username, repo = "roweb2",
+         content = post_content,
+         path = paste0("content/blog/", file_name),
+         message = "am I adding a post?")
+
+  # copy images
 }
