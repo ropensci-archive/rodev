4a0cc973caa3610014be0773981a1b6b30975058
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-04-16 15:58:07 +0200

    add function to prepare a fork

--- R/blog_posts.R
+++ R/blog_posts.R
@@ -62,8 +62,27 @@ create_post_project <- function(slug, type = "Rmd"){
 }
 
 
+#' Prepare fork
+#'
+#' Based on project with post.md and images in images/,
+#' forks the website repo, put the post content in the right place,
+#' the images in the right place and correct the path to them in the
+#' post.
+#'
+#' @param post_project_path Path to the project where the post is.
+#' @param gh_username GitHub username.
+#'
+#' @details This relies on your having a GitHub PAT.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 prepare_fork <- function(post_project_path, gh_username = "maelle"){
   # if fork exist update it
+  gh::gh("POST /repos/:owner/:repo/forks",
+         owner = "ropensci",
+         repo = "roweb2")
 
   # put post in right folder
   # with name deduced from date and slug
@@ -75,15 +94,48 @@ prepare_fork <- function(post_project_path, gh_username = "maelle"){
   file_name <- paste0(date, "-", slug, ".md")
 
   # replace image links
+  post <- readLines(post_path)
+  post <- stringr::str_replace_all(post,
+                                   "images\\/",
+                                   paste0("/img/blog-images/",
+                                          date, "-", slug,
+                                          "/"))
 
-  post_content <- readBin(post_path, "raw",
-                                   file.info(post_path)[1, "size"])
+  temp_post_path <- tempfile()
+  writeLines(post, temp_post_path)
+
+  # raw post
+  post_content <- readBin(temp_post_path, "raw",
+                          file.info(temp_post_path)[1, "size"])
 
   gh::gh("PUT /repos/:owner/:repo/contents/:path",
          owner = gh_username, repo = "roweb2",
          content = post_content,
          path = paste0("content/blog/", file_name),
-         message = "am I adding a post?")
+         message = "add post")
 
   # copy images
+  images <- dir(file.path(post_project_path, "images"),
+                full.names = TRUE)
+  lapply(images, post_image,
+         post_project_path = post_project_path,
+         gh_username = gh_username,
+         date = date, slug = slug)
+}
+
+
+post_image <- function(image_path, post_project_path,
+                       gh_username, date, slug){
+  filename <- gsub(".*\\/", "", image_path)
+
+  image_content<- readBin(image_path, "raw",
+                           file.info(image_path)[1, "size"])
+
+  gh::gh("PUT /repos/:owner/:repo/contents/:path",
+         owner = gh_username, repo = "roweb2",
+         content = image_content,
+         path = paste0("themes/ropensci/static/img/blog-images/",
+                       date, "-", slug,
+                       "/", filename),
+         message = "add image")
 }
