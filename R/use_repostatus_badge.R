cd17c26e5be595ac5d755475a8eb3a20f1953960
Maëlle Salmon <maelle@itsalocke.com>
2018-05-09 14:33:12 +0200

    will work without loading the whole package

--- R/use_repostatus_badge.R
+++ R/use_repostatus_badge.R
@@ -10,11 +10,11 @@
 use_repostatus_badge <- function(status){
   # inspired by https://github.com/r-lib/usethis/blob/master/R/badge.R
   # so probably needs proper credit!
-  if(!status %in% repostatus_badges$status){
+  if(!status %in% rodev::repostatus_badges$status){
     stop("The status you input is not a proper repostatus.org status at least when this package version was released. Please have a look at repostatus_badges") # nolint
   }
 
-  md_code <- repostatus_badges$md_code[repostatus_badges$status == status]
+  md_code <- rodev::repostatus_badges$md_code[rodev::repostatus_badges$status == status]
   # probably not a good way to use usethis
   usethis:::todo(
     "Add a ",
