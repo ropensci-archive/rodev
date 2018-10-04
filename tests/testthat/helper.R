b1de9e7d93366c402396f1be64e3c0570634a3ce
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-10-04 15:40:37 +0200

    who knows

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,7 +1,7 @@
 # from desc
 # https://github.com/r-lib/desc/blob/4f60833fdb6d1aae4cbf09b7eb293c5fa0770e5c/tests/testthat/helper.R#L2
 temp_desc <- function(file = "D2") {
-  tmp <- tempdir()
+  tmp <- tempdir(check = TRUE)
   file.copy(file, file.path(tmp, "DESCRIPTION"))
   file.path(tmp, "DESCRIPTION")
 }
