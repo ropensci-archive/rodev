20b43d8414983426c84250693c4cb918109b17f2
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-05-08 13:11:50 +0200

    fix test?

--- tests/testthat/test-optout_rodocs.R
+++ tests/testthat/test-optout_rodocs.R
@@ -1,5 +1,7 @@
 test_that("optout_rodocs works", {
+  usethis::proj_set(force = TRUE)
   optout_rodocs()
   on.exit(file.remove(".norodocs"))
   expect_true(file.exists(".norodocs"))
+  usethis::proj_set(NULL)
 })
