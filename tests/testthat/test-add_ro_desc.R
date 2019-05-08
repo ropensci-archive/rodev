83f27d53d2f1922514c87dcfabb2db0f502ee1b3
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-05-08 11:01:08 +0200

    better?

--- tests/testthat/test-add_ro_desc.R
+++ tests/testthat/test-add_ro_desc.R
@@ -1,9 +1,8 @@
 context("test-add_ro_desc")
 
 test_that("add_ro_desc works", {
-  dir.create("testd")
-  d <- desc::desc()
-  d$write(file = file.path("testd", "DESCRIPTION"))
+  d <- testthat::test_path("ok_description")
+  fs::dir_copy(d, "testd")
   on.exit(fs::dir_delete("testd"))
   add_ro_desc("testd")
   expect_match(desc::desc_get_field("Description",
