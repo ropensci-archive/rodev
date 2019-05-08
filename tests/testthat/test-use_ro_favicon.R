5e53d279d6eec0843275979f064e633728e1eb24
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-05-08 13:46:24 +0200

    oops

--- tests/testthat/test-use_ro_favicon.R
+++ tests/testthat/test-use_ro_favicon.R
@@ -1,6 +1,6 @@
 test_that("use_ro_favicon works", {
-  use_ro_favicon()
   usethis::proj_set(force = TRUE)
+  use_ro_favicon()
   expect_true(dir.exists(file.path(usethis::proj_get(),
                                    "pkgdown")))
   fs::dir_delete(file.path(usethis::proj_get(),
