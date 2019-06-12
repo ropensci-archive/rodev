c5f539a3d79fe739b7ba74301fb9c39163a9ade3
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-06-12 13:21:33 +0200

    rm useless test

--- tests/testthat/test-optout_rodocs.R
+++
@@ -1,7 +0,0 @@
-test_that("optout_rodocs works", {
-  usethis::proj_set(force = TRUE)
-  optout_rodocs()
-  on.exit(file.remove(".norodocs"))
-  expect_true(file.exists(".norodocs"))
-  usethis::proj_set(NULL)
-})
