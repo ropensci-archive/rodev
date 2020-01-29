6995b4f38c1e95efb365e92573b21aed129b1ea8
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-29 15:21:46 +0100

    fix test

--- tests/testthat/test-add_ro_desc.R
+++ tests/testthat/test-add_ro_desc.R
@@ -1,11 +1,7 @@
 context("test-add_ro_desc")
 
 test_that("add_ro_desc works", {
-  d <- testthat::test_path("ok_description")
-  fs::dir_copy(d, "testd")
-  on.exit(fs::dir_delete("testd"))
-  add_ro_desc("testd")
-  expect_match(desc::desc_get_field("Description",
-                                    file = "testd/DESCRIPTION"),
-               "has been peer-reviewed by rOpenSci" )
+  expect_error(
+    add_ro_desc(),
+    "no longer")
 })
