e61d76fed31b8faa0f49324b97cfdf3d10ba2dc1
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-29 14:51:37 +0100

    no longer add mention to desc

--- R/add_ro_desc.R
+++ R/add_ro_desc.R
@@ -1,17 +1,11 @@
-#' Add rOpenSci review mention to DESCRIPTION
+#' Add rOpenSci review mention to DESCRIPTION (no longer recommended)
 #'
-#' Add rOpenSci review mention to DESCRIPTION. Only run this
-#' after your package has been approved.
+#' No longer recommended!
 #'
 #' @param path path to package
 #'
 #' @export
 add_ro_desc <- function(path = usethis::proj_get()){
-  file <- file.path(path, "DESCRIPTION")
-  version <- desc::desc_get_version(file = file)
-  description <- desc::desc_get_field("Description",
-                                      file = file)
-  new_description <- glue::glue("{description}\n   This package has been peer-reviewed by rOpenSci (v. {version}).")
-  desc::desc_set(Description = new_description,
-                 file = file)
+ stop("We no longer recommend adding an rOpenSci review mention to DESCRIPTION.",
+      call. = FALSE)
 }
